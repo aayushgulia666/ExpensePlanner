e8bbc4a772673ac57fbd710639bea925f60f8503
Aayush Gulia <aayushgulia07@outlook.com>
2022-10-23 19:55:27 +0530

    Initial Commit.

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Container(
+              child: Text('CHART!'),
+              width: double.infinity,
+              height: 150,
+            ),
+            color: Colors.green,
+          ),
+          Card(
+            child: Text('List of TXN\'s'),
+          )
+        ],
+      ),
+    );
+  }
+}
